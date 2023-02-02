5c588bc92dbe3c631d193dea01c2ab6ec8b8adf4
TerryDiaz <adiarub160@iesmartinezm.es>
2023-02-02 16:44:26 +0100

    hecho completo T5, repasando para examen T5

--- PreExamenT5/constantes.php
+++
@@ -1,7 +0,0 @@
-<?php
-const dsn = 'mysql:dbname=dwes2;host=127.0.0.1';
-const user = 'dwes2';
-const password = 'abc123.';
-
-
-?>
--- PreExamenT5/contraseñas.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo password_hash('pepe', PASSWORD_DEFAULT);
-
